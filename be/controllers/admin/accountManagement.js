cd3a2569705c2e74869199a83bf535ef61100d3b
Hao <quanghao040102@gmail.com>
2025-03-22 22:25:25 +0700

    fix_lock_account

--- be/controllers/admin/accountManagement.js
+++ be/controllers/admin/accountManagement.js
@@ -18,11 +18,14 @@ async function islockAccount(req, res) {
         const account = await AccountRepository.getAccountById(id);
         if (!account) return res.status(404).json({ message: "Tài khoản không tồn tại" });
 
-        account.isLocked = !account.isLocked;
-        await account.save();
+        const updatedAccount = await Account.findByIdAndUpdate(
+            id,
+            { $set: { isLocked: !account.isLocked } },
+            { new: true, runValidators: false }
+        );
 
         res.json({
-            message: `Tài khoản ${account.isLocked ? "đã bị khóa" : "đã được mở khóa"}`
+            message: `Tài khoản ${updatedAccount.isLocked ? "đã bị khóa" : "đã được mở khóa"}`
         });
     } catch (error) {
         res.status(500).json({ message: error.message });
